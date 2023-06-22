a48383aa2e0a672ce8870e7ae6a4fed6e312e45e
GianbaBotassi <gianba89@gmail.com>
2023-06-22 15:44:58 +0200

    Problem with axios CORS

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,42 @@
 <script >
+
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      // urlPHP: "http://localhost/index.php",
+      tasks: [],
+      // headers: "'Content-Type': 'multipart/form-data'"
+    }
+  },
+  mounted() {
+    axios.get('http://localhost/index.php')
+      .then(response => {
+        console.log(response.data);
+      })
+  }
+}
 </script>
+
+
 
 <template>
   <h1>Todo List</h1>
   <ul class="container">
-    <li>
+    <li v-for="ciao in 4">
       <span>Ciao</span>
       <i class="fa-solid fa-trash"></i>
     </li>
-    <li>
-      <span>Ciao</span>
-      <span>Ciao</span>
-    </li>
-    <li>
-      <span>Ciao</span>
-      <span>Ciao</span>
-    </li>
+
   </ul>
   <form>
 
-    <input type="text" name="new" id="">
+    <input type="text" name="newtask" id="newtask" placeholder="Inserisci un nuovo task..">
+    <input type="submit" value="Inserisci">
+
 
   </form>
 </template>
 
-<style>
-.container {
-  background-color: white;
-  width: 300px;
-  border-radius: 10px;
-  color: black;
-}
-
-ul {
-  list-style: none;
-  text-align: left;
-  padding: 0;
-
-}
-
-li {
-  padding: 20px;
-  border-bottom: 1px solid black;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-i {
-  padding: 5px;
-  background-color: rgb(195, 78, 78);
-  color: white;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-i:hover {
-  background-color: red;
-}
-</style>
+<style></style>
