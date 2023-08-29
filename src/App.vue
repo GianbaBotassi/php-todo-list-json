781ba566909abe7300168d5a3d9270bd116b20c1
GianbaBotassi <gianba89@gmail.com>
2023-08-29 15:53:05 +0200

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,93 @@
 <script >
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      urlIndexPhp: "http://localhost/Esercizi%20BackEnd/php-todo-list-json/tmp/index.php",
-      urlActionTaskPhp: 'http://localhost/Esercizi%20BackEnd/php-todo-list-json/tmp/actionTask.php',
+      urlIndexPhp:
+        "http://localhost/Esercizi%20BackEnd/php-todo-list-json/tmp/index.php",
+      urlActionTaskPhp:
+        "http://localhost/Esercizi%20BackEnd/php-todo-list-json/tmp/actionTask.php",
       newTask: "",
       tasks: [],
       headers: {
-        headers: { 'Content-Type': 'multipart/form-data' }
-      }
-    }
+        headers: { "Content-Type": "multipart/form-data" },
+      },
+    };
   },
   mounted() {
-    axios.get(this.urlIndexPhp)
-      .then(response => {
-        this.tasks = response.data;   //prende all'inizio task list da server PHP
+    axios
+      .get(this.urlIndexPhp)
+      .then((response) => {
+        this.tasks = response.data; //prende all'inizio task list da server PHP
       })
+      .catch((err) => {
+        console.log("err:", err);
+      });
   },
   methods: {
-    addTask() {   //metodo per aggiungere nuova task
+    addTask() {
+      //metodo per aggiungere nuova task
       const data = {
         name: this.newTask,
-        action: "add"
+        action: "add",
+      };
+      if (this.newTask === "") {
+        //return in caso di campo vuoto
+        return;
       }
-      if (this.newTask === "") {    //return in caso di campo vuoto
-        return
-      }
-      this.axiosPostCall(data)
+      this.axiosPostCall(data);
     },
-    deleteTask(idx) {   //metodo per eliminare task
+    deleteTask(idx) {
+      //metodo per eliminare task
       const data = {
         index: idx,
-        action: "delete"
+        action: "delete",
       };
-      this.axiosPostCall(data)
+      this.axiosPostCall(data);
     },
-    toggleTask(idx) {   //metodo per togglare task.done
+    toggleTask(idx) {
+      //metodo per togglare task.done
       const data = {
         index: idx,
-        action: "toggle"
+        action: "toggle",
       };
-      this.axiosPostCall(data)
+      this.axiosPostCall(data);
     },
     axiosPostCall(data) {
-      axios.post(this.urlActionTaskPhp, data, this.headers)
-        .then(response => {
+      axios
+        .post(this.urlActionTaskPhp, data, this.headers)
+        .then((response) => {
           this.tasks = response.data;
-          this.newTask = ""
+          this.newTask = "";
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
   <h1>Todo List</h1>
   <ul class="container">
     <li v-for="(task, idx) in tasks" :key="idx">
-      <span :class="task.done ? 'done' : ''" @click="toggleTask(idx)">{{ task.name }}</span>
+      <span :class="task.done ? 'done' : ''" @click="toggleTask(idx)">{{
+        task.name
+      }}</span>
       <i class="fa-solid fa-trash" @click="deleteTask(idx)"></i>
     </li>
-
   </ul>
   <form @submit.prevent="addTask">
-    <input type="text" name="newtask" id="newtask" v-model.trim="newTask" placeholder="Inserisci un nuovo task..">
-    <input type="submit" value="Inserisci">
+    <input
+      type="text"
+      name="newtask"
+      id="newtask"
+      v-model.trim="newTask"
+      placeholder="Inserisci un nuovo task.."
+    />
+    <input type="submit" value="Inserisci" />
   </form>
 </template>
 
